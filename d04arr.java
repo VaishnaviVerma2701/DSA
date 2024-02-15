c1357ce881b93fbed3b7c1da414ca7e452fd13aa
VaishnaviVerma2701 <vaishnaviverma2701@gmail.com>
2024-02-16 01:45:56 +0530

    DSA with java

--- d04arr.java
+++ d04arr.java
@@ -1,6 +1,6 @@
 //import java.util.Scanner;
 import java.util.Arrays;
-class d04arr{
+class arr{
      public static void main(String[] args) {
         
        // Scanner sc=new Scanner(System.in);
